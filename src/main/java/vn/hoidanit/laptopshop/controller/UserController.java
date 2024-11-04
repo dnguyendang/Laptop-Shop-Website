7690f4e5419932c767c244cc732e1f07dd53cb82
Dangg Dungg <dungduongthanhson@gmail.com>
2024-11-04 21:07:14 +0700

    create user

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,35 +9,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
     // DI: dependency injection
-    private UserService userService;
+    private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
-        model.addAttribute("dung", "hello dung");
+        Iterable<User> arrUsers = this.userService.getAllUsersByEmail("dungduongthanhson@gmail.com");
+        System.out.println(arrUsers);
         return "hello";
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        Iterable<User> users = this.userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/create") // GET
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User newUser) {
-        System.out.println(" run here " + newUser);
-        return "hello";
+        this.userService.handleSaveUser(newUser);
+        return "redirect:/admin/user";
     }
 }
---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package vn.hoidanit.laptopshop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.hoidanit.laptopshop.domain.User;
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User save(User dung);
+
+    List<User> findByEmail(String email);
+}
